0b5d74a31958d2ef7f3fb4601957cca235d599b6
YuhangXiong <yxiong4@ualberta.ca>
2019-09-26 18:03:33 -0600

    add reduce ingredients function to the app

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -14,6 +14,8 @@ const buildControls = (props) =>(
         {controls.map(control=>(
            <BuildControl key={control.label} label={control.label}
            added = {()=>props.ingredientAdded(control.type)}
+           removed = {()=>props.ingredientRemoved(control.type)}
+           disabled = {props.disabled[control.type]}
           /> 
         ))}
     </div>
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -40,16 +40,37 @@ class BurgerBuilder extends Component{
         this.setState({totalPrice:updatedPrice,ingredients:updatedIngredients});
     }
 
-    // removeIngredientHandler = (type) =>{
-        
-    // }
+    removeIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        if(oldCount===0){
+            return;
+        }
+        const updatedCount = oldCount-1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+        const priceMinus  = INGREDIENT_PRICE[type];
+        const oldPrice = this.state.totalPrice;
+        const updatedPrice = oldPrice - priceMinus;
+        this.setState({totalPrice:updatedPrice,ingredients:updatedIngredients});
+    }
 
     render(){
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for(let key in disabledInfo){
+            disabledInfo[key]= disabledInfo[key]<=0;
+        }
+
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                ingredientAdded = {this.addIngredientHandler}/>
+                ingredientAdded = {this.addIngredientHandler}
+                ingredientRemoved = {this.removeIngredientHandler}
+                disabled = {disabledInfo}/>
                 
             </Aux>
         );
